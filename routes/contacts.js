9d3f7f87fb7ecead6eb55e0e12e0ee8f6c5acbb3
Howl1935 <travisphawley@gmail.com>
2021-12-07 18:01:51 -0800

    Express server setup with initial routes

---
+++ routes/contacts.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const router = express.Router();
+
+// @route   GET api/contacts
+// @desc    Get all user's contacts
+// @access  Private
+router.get('/', (req, res) => {
+    res.send('Get all contacts');
+});
+
+
+// @route   POST api/contacts
+// @desc    Add new Contact
+// @access  Private
+router.post('/', (req, res) => {
+    res.send('Add contact');
+});
+
+
+
+// @route   PUT api/contacts/:id
+// @desc    Update contact
+// @access  Private
+router.put('/:id', (req, res) => {
+    res.send('Update contact');
+});
+
+
+
+// @route   DELETE api/contacts/:id
+// @desc    Register a user
+// @access  Public
+router.delete('/:id', (req, res) => {
+    res.send('Deletes a contact');
+});
+
+module.exports = router;
